bd2d6891d9da6aee38b1504d647433e8fd8b79d3
Thakur-Singh123 <kapoorthakur906.com>
2025-10-01 11:42:08 +0530

    tete

--- app/Http/Controllers/Manager/DoctorController.php
+++ app/Http/Controllers/Manager/DoctorController.php
@@ -61,7 +61,7 @@ class DoctorController extends Controller
             'location' => $request->location,
             'remarks' => $request->remarks,
             'picture' => $filename,
-            'status' => 'active',
+            'status' => 'pending',
         ]);
         //Check if doctor created or not
         if ($is_create_doctor) {
@@ -145,7 +145,7 @@ class DoctorController extends Controller
                 'location' => $request->location,
                 'remarks' => $request->remarks,
                 'picture' => $filename,
-                'status' => $request->status,
+                'status' => 'pending',
             ]);
             //Check if doctor updated or not
             if ($is_updated_doctor) {
@@ -168,7 +168,7 @@ class DoctorController extends Controller
                 'doctor_contact' => $request->doctor_contact,
                 'location' => $request->location,
                 'remarks' => $request->remarks,
-                'status' => $request->status,
+                'status' => 'pending',
             ]);
             //Check if doctor updated or not
             if ($is_updated_doctor) {

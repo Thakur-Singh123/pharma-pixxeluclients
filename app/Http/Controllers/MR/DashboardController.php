e0832a5f41cffe40781af82b20de1605a77ad8f0
Thakur-Singh123 <kapoorthakur906.com>
2025-10-17 13:20:09 +0530

    ereR

--- app/Http/Controllers/MR/DashboardController.php
+++ app/Http/Controllers/MR/DashboardController.php
@@ -4,9 +4,12 @@ namespace App\Http\Controllers\MR;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Visit;
+use App\Models\Task; 
+use App\Models\MRAttendance;
+use App\Models\Sale;
 use App\Models\Client;
 use App\Models\TADARecords;
-use App\Models\Visit;
 use App\Models\DailyReport;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Auth;
@@ -15,16 +18,27 @@ class DashboardController extends Controller
 {
     //Function for show dashboard
     public function dashboard() {
+        //Get visitor
+        $total_visits = Visit::where('mr_id', Auth::id())->count();
+        //Get completed tasks
+        $total_completed_task = Task::where('mr_id', Auth::id())->where('status', 'completed')->count();
+        //Get attendances
+        $total_attendances = MRAttendance::where('user_id', Auth::id())->count();
+        //Get sales
+        $total_sales = Sale::where('user_id', Auth::id())->count();
+
         //Get clients
         $is_approved = Client::where('mr_id', Auth::id())->where('status', 'Approved')->count();
         $is_pending = Client::where('mr_id', Auth::id())->where('status', 'Pending')->count();
         $is_reject = Client::where('mr_id', Auth::id())->where('status', 'Reject')->count();
+
         //Get tada record
         $bus = TADARecords::where('mr_id', Auth::id())->where('mode_of_travel', 'Bus')->count();
         $train = TADARecords::where('mr_id', Auth::id())->where('mode_of_travel', 'Train')->count();
         $flight = TADARecords::where('mr_id', Auth::id())->where('mode_of_travel', 'Flight')->count();
         $car = TADARecords::where('mr_id', Auth::id())->where('mode_of_travel', 'Car')->count();
         $bike = TADARecords::where('mr_id', Auth::id())->where('mode_of_travel', 'Bike')->count();
+
         //Get current week
         $startOfWeek = Carbon::now()->startOfWeek();
         $endOfWeek   = Carbon::now()->endOfWeek();
@@ -34,6 +48,7 @@ class DashboardController extends Controller
             ->groupBy('day')
             ->get()
             ->keyBy('day');
+
         //Get weeks
         $weeklyData = [];
         for ($date = $startOfWeek->copy(); $date->lte($endOfWeek); $date->addDay()) {
@@ -42,6 +57,7 @@ class DashboardController extends Controller
                 'total' => $DailyReport->has($date->toDateString()) ? $DailyReport[$date->toDateString()]->total : 0,
             ];
         }
+
         //Get monthly visits
         $visits = Visit::where('mr_id', Auth::id())->selectRaw('MONTH(visit_date) as month, COUNT(*) as total')
             ->groupBy('month')
@@ -55,6 +71,6 @@ class DashboardController extends Controller
             ];
         }
         
-        return view('mr.dashboard', compact('is_approved','is_pending','is_reject','bus','train','flight','car','bike','weeklyData','monthlyData'));
+        return view('mr.dashboard', compact('total_visits','total_completed_task','total_attendances','total_sales','is_approved','is_pending','is_reject','bus','train','flight','car','bike','weeklyData','monthlyData'));
     }
 }
--- app/Notifications/TourStatusNotification.php
+++ app/Notifications/TourStatusNotification.php
@@ -9,32 +9,35 @@ class TourStatusNotification extends Notification
 {
     use Queueable;
 
+    //Get tour plan 
     protected $tour_plan;
-
-    public function __construct($tour_plan)
-    {
+    
+    //Function for construct
+    public function __construct($tour_plan) {
         $this->tour_plan = $tour_plan;
     }
 
-    public function via(object $notifiable): array
-    {
+    //Function for db notification
+    public function via(object $notifiable): array {
         return ['database'];
     }
-
-    public function toArray(object $notifiable): array
-    {
+    
+    //Function for get tour status
+    public function toArray(object $notifiable): array  {
+        //Get status
         $status = $this->tour_plan->approval_status; 
         $message = '';
         $icon = '';
-
+        //Check if status approved or not
         if($status === 'Approved') {
             $message = "Your tour plan titled '{$this->tour_plan->title}' has been approved by the manager.";
             $icon = 'fas fa-plane';
+        //Check if status rejected or not
         } elseif($status === 'Rejected') {
             $message = "Oops! Your tour plan titled '{$this->tour_plan->title}' has been rejected by the manager. Please review the plan, make necessary changes, and resend it for approval.";
             $icon = 'fas fa-times-circle text-danger';
         }
-
+        //Get data
         return [
             'tour_id' => $this->tour_plan->id,
             'title' => $this->tour_plan->title,

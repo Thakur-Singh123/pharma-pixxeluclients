94b5e7adaae0246cff5632c84f23a45aadc5a915
abhikumar0607 <abhikumar06071998@gmail.com>
2025-11-13 11:03:25 +0530

    Added purchase order module

--- routes/api/mr.php
+++ routes/api/mr.php
@@ -10,7 +10,7 @@ Route::prefix('mr')->middleware(['auth:sanctum', 'mr'])->group(function () {
     Route::get('/account', [App\Http\Controllers\Api\MR\ProfileController::class, 'account']);
     Route::post('/update-account', [App\Http\Controllers\Api\MR\ProfileController::class, 'update_account']);
     //Attendances
-    Route::get('/attendance', [App\Http\Controllers\Api\MR\AttendanceController::class, 'today']);
+    Route::get('/attendance', [App\Http\Controllers\Api\MR\AttendanceController::class, 'index']);
     Route::post('/attendance/check-in', [App\Http\Controllers\Api\MR\AttendanceController::class, 'checkIn']);  
     Route::post('/attendance/check-out', [App\Http\Controllers\Api\MR\AttendanceController::class, 'checkOut']);
     Route::get('/attendance/month', [App\Http\Controllers\Api\MR\AttendanceController::class, 'month']); 

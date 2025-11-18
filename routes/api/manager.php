f3615c81c5d0ab002973b8097868b2d02908c5a6
abhikumar0607 <abhikumar06071998@gmail.com>
2025-11-18 13:10:28 +0530

    dd

--- routes/api/manager.php
+++ routes/api/manager.php
@@ -13,4 +13,11 @@ Route::prefix('manager')->middleware(['ensure.token','auth:sanctum', 'manager'])
     Route::post('/mrs', [App\Http\Controllers\Api\Manager\MRController::class, 'store']);
     Route::patch('/mrs/{id}', [App\Http\Controllers\Api\Manager\MRController::class, 'update']);
     Route::delete('/mrs/{id}', [App\Http\Controllers\Api\Manager\MRController::class, 'destroy']);
+
+    //attendances
+    Route::get('/attendance/{type?}', [App\Http\Controllers\Api\Manager\AttendanceController::class, 'index']);
+    //User status management
+    Route::get('/users', [App\Http\Controllers\Api\Manager\UserStatusController::class, 'index']);
+    Route::post('/users/approve/{id}', [App\Http\Controllers\Api\Manager\UserStatusController::class, 'approve']);
+    Route::post('/users/suspend/{id}', [App\Http\Controllers\Api\Manager\UserStatusController::class, 'suspend']);
 });

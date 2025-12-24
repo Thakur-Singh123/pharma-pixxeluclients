377073f58adc15dcf76ebe91de56a987b2daa220
Thakur-Singh123 <kapoorthakur906.com>
2025-12-24 10:02:18 +0530

    hjfghf

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,7 +2,6 @@
 @section('content')
   <div class="container">
           <div class="page-inner">
-
             <div class="row">
               <div class="col-sm-6 col-md-3">
                 <div class="card card-stats card-round">
